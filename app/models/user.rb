65810b28f0f5dc38675b6c50fcd8a79660d8ae94
kyoko-u <lapis9.3orchid4.24@gmail.com>
2024-07-21 16:14:04 +0900

    バリデーションの正規表現の追加とエラーコードの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カタカナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
   validates :birthday, presence: true
 
-  validate :password_complexity
-
-  private
-
-  def password_complexity
-    return if password.blank? || password.match(/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i)
-
-  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
 end
