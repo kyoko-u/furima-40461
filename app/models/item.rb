5544d3350d2d56f2798ab25064ed5c3047c12047
kyoko-u <lapis9.3orchid4.24@gmail.com>
2024-07-29 06:25:06 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,17 @@ class Item < ApplicationRecord
   belongs_to :Prefecture
   belongs_to :schedule_delivery
   belongs_to :shipping_free_status  
+
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_free_status_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :schedule_delivery_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :user, presence: true
+  validates :image, presence: true
+
+
 end
