c78db6e6ee3353a7cf4a2e45450792d289ebe0f8
kyoko-u <lapis9.3orchid4.24@gmail.com>
2024-07-30 06:12:05 +0900

    商品出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+
+  def new
+  end
 
   private
 
   def item_params
     params.require(:item).permit(:image).merge(user_id: current_user.id)
   end
-    
+
 end
