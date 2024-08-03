b4d14913b83759803eb9c64aed6e29e123e9ffdf
kyoko-u <lapis9.3orchid4.24@gmail.com>
2024-08-03 17:05:41 +0900

    itemモデル単体テストコードを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,8 +16,8 @@ class Item < ApplicationRecord
   validates :shipping_free_status_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :schedule_delivery_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },format: { with: /\A[0-9]+\z/, message: "半角数字を使用してください" }
-  validates :user, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "300~9999999の半角数字を使用してください"  },format: { with: /\A[0-9]+\z/, message: "300~9999999の半角数字を使用してください" }
+  # validates :user, presence: true
   validates :image, presence: true
 
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,97 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品新規登録' do
+    context '新規登録できるとき' do
+      it 'name、info、category_id、condition_id、shipping_free_status_id、prefecture_id、schedule_delivery_id、price、imageが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+      it 'priceが半角数字であれば登録できる' do
+        @item.price = '1000'
+        expect(@item).to be_valid
+      end
+      it 'priceが¥300～¥9,999,999であれば登録できる' do
+        @item.price = 300
+        expect(@item).to be_valid
+
+        @item.price = 9999999
+        expect(@item).to be_valid
+
+        @item.price = 5000
+        expect(@item).to be_valid
+      end      
+    end
+
+    context '新規登録できないとき' do
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end      
+      it 'infoが空では登録できない' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+      it 'category_idが空では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'condition_idが空では登録できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'shipping_free_status_idが空では登録できない' do
+        @item.shipping_free_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping free status can't be blank")
+      end
+      it 'prefecture_idが空では登録できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'schedule_delivery_idが空では登録できない' do
+        @item.schedule_delivery_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Schedule delivery can't be blank")
+      end
+      it 'priceが空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが¥300未満では登録できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price 300~9999999の半角数字を使用してください')
+      end
+      it 'priceが¥9,999,999を超えると登録できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price 300~9999999の半角数字を使用してください')
+      end
+      it 'priceが全角では登録できない' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price 300~9999999の半角数字を使用してください')
+      end
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
+end  
