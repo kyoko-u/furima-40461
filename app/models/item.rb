4bf89409fac115a783fa849cd5e68c4a6c41b13a
kyoko-u <lapis9.3orchid4.24@gmail.com>
2024-08-09 11:02:22 +0900

    商品詳細表示ページを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,4 +20,5 @@ class Item < ApplicationRecord
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: '300~9999999の半角数字を使用してください' }
   # validates :user, presence: true
   validates :image, presence: true
+
 end
