e7efc94303494ba9caeb65e5eac473e5c90aa009
kyoko-u <lapis9.3orchid4.24@gmail.com>
2024-08-13 05:23:04 +0900

    レビューコメントに沿って修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
 
   def new
     @item = Item.new
@@ -35,7 +35,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     @item.destroy
     redirect_to root_path
   end
