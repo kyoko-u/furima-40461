9a4364e9609e8e882688fa3a13ac7945965411b6
kyoko-u <lapis9.3orchid4.24@gmail.com>
2024-08-16 23:34:24 +0900

    フォームの情報を保存できるように実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,26 @@ class OrdersController < ApplicationController
     @order_shipping_address = OrderShippingAddress.new
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    @order_shipping_address = OrderShippingAddress.new(order_shipping_address_params)
+
+    if @order_shipping_address.valid?
+      @order = Order.create(user_id: current_user.id, item_id: params[:item_id])
+      @order_shipping_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id]) 
+      render :index, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
+  def order_shipping_address_params
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+  
 end
 
 
